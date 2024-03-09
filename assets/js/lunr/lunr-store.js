52e7b7c9d44d6fcbbfe229f335a5e33484826683
SpinStabilized <SpinStabilized@users.noreply.github.com>
2024-03-09 16:10:44 +0000

    jekyll build from Action ff1009962fceb18415dbc3b8ba527d4557b0dba4

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "A New Site",
+        "excerpt":"Starting with a new github hosted page. I am going to start updating it with  more information on my personal projects, a resume, and maybe some information about things that interest me.   Watch this space for more coming soon!  ","categories": ["site"],
+        "tags": [],
+        "url": "/site/2023/02/02/a-new-site.html",
+        "teaser": null
+      },{
+        "title": "Jekyll and Github Actions",
+        "excerpt":"Here’s to hoping this posts and everything is figured out. All I wanted to do  was move to a GitHub Actions based Jekyll deployment so I could use additional  plugins and such. It has been a journey. I need to write it up at some point.  ","categories": ["site"],
+        "tags": [],
+        "url": "/site/2023/02/06/jekyll-github-actions.html",
+        "teaser": null
+      }]
