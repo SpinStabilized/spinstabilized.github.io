bdb1f536be892c58cb8ce8ae818786371132f888
SpinStabilized <SpinStabilized@users.noreply.github.com>
2025-10-10 16:41:42 +0000

    jekyll build from Action cb9de693de19f4424741107f2c55a6e9f549ad9d

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "A New Site",
+        "excerpt":"Starting with a new github hosted page. I am going to start updating it with  more information on my personal projects, a resume, and maybe some information about things that interest me.   Watch this space for more coming soon!  ","categories": ["site"],
+        "tags": [],
+        "url": "/site/2023/02/02/a-new-site.html",
+        "teaser": null
+      },{
+        "title": "Jekyll and Github Actions",
+        "excerpt":"Here’s to hoping this posts and everything is figured out. All I wanted to do  was move to a GitHub Actions based Jekyll deployment so I could use additional  plugins and such. It has been a journey. I need to write it up at some point.  ","categories": ["site"],
+        "tags": [],
+        "url": "/site/2023/02/06/jekyll-github-actions.html",
+        "teaser": null
+      },{
+        "title": "Amateur Radio Start",
+        "excerpt":"Getting started into Amateur Radio. Currently my call is KD3BTI and I’ve started a separate section of this site for me to post stuff specific to all things Amateur Radio and Ham.   KD3BTI Home  ","categories": ["amateur radio"],
+        "tags": [],
+        "url": "/amateur%20radio/2025/10/10/amateur-radio-start.html",
+        "teaser": null
+      }]
